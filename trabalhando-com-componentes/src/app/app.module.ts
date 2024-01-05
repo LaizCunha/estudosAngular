72010852e97bd3befa7ec54aeedbc8323196929a
Laiz <laizsc.89@gmail.com>
2024-01-05 15:55:02 -0300

    Implementando diretiva ngModel

--- trabalhando-com-componentes/src/app/app.module.ts
+++ trabalhando-com-componentes/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,17 +7,20 @@ import { AppComponent } from './app.component';
 import { Hellocomponent } from './components/hello.component';
 import { MenuComponent } from './components/menu.component';
 import { Button } from './components/button.component';
+import { EntryDataComponent } from './components/entry-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Hellocomponent,
     MenuComponent,
-    Button
+    Button,
+    EntryDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
